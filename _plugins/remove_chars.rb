60548d79b1b297a0c24b8dfb94fbcd6d5be2cd71
Ryan Wolfslayer <rdw717756@gmail.com>
2025-12-18 23:00:33 +0000

    Fix remove_chars.rb regex for Ruby 3.2 compatibility

--- _plugins/remove_chars.rb
+++ _plugins/remove_chars.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 module Jekyll
-    module RemoveCharsFilter
-      def remove_chars(input)
-        input.gsub(/[\u{1F600}-\u{1F6FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F900}-\u{1F9FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|\xE2[\x80-\x8F][\x80-\xBF]|\xEF\xB8[\x8F-\x8F]|\xE2\x9C[\x82-\x82]|\xE2\x9D[\xA4-\xA4]|\xE2\x99[\xA0-\xAF]|\xE2\x99[\xB0-\xBF]|\xE3[\x80-\xBF][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF][\x80-\xBF]|[*<>:;#$%^&{}@!~`=]/, ' ')
-          .gsub(/\s+/, ' ')
-          .gsub(/\n+/, ' ')
-      end
+  module RemoveCharsFilter
+    # Remove emojis and special characters from text for search indexing
+    EMOJI_PATTERN = /[\u{1F300}-\u{1F9FF}]|[\u{2600}-\u{27BF}]|[\u{FE00}-\u{FE0F}]|[\u{1F000}-\u{1FFFF}]/
+    SPECIAL_CHARS = /[*<>:;#$%^&{}@!~`=]/
+
+    def remove_chars(input)
+      return '' if input.nil?
+
+      input
+        .gsub(EMOJI_PATTERN, ' ')
+        .gsub(SPECIAL_CHARS, ' ')
+        .gsub(/\s+/, ' ')
+        .strip
     end
   end
-  
-  Liquid::Template.register_filter(Jekyll::RemoveCharsFilter)
+end
+
+Liquid::Template.register_filter(Jekyll::RemoveCharsFilter)
